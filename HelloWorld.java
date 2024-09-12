3eca72c68f25dc8106e8b5f7e4682196fc4170af
Anand.S <vsanand18@gmail.com>
2024-09-12 17:32:06 +0530

    Updated

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.err.println("Error Message");
+        System.out.println("test");
     }
 }
